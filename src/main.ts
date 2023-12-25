76add8b6dbc1d1bc34039d564ca480f44ceeb7a4
nikoazax2 <nicolasage38@orange.fr>
2023-12-25 17:52:43 +0100

    add

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://sagenicolas.fr',
+        origin: 'http://89.116.228.185:3000',
     })
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ extended: true, limit: '50mb' }));
