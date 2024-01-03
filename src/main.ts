eb85a9375fb6be4d44c3d0d4ba888a527e7b8f05
nikoazax2 <nicolasage38@orange.fr>
2024-01-03 20:44:41 +0100

    add

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,20 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: ['http://localhost:8080', 'http://89.116.228.185', 'https://sagenicolas.fr', 'http://89.116.228.185/commerceclient/'],
-    })
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ extended: true, limit: '50mb' }));
-    const config = new DocumentBuilder()
-        .setTitle('Ecommerce example')
-        .setDescription('The ecommerce API control')
-        .setVersion('1.0')
-        .addTag('ecommerce')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-    await app.listen(3000);
+    const app = await NestFactory.create(AppModule, { cors: true });
+app.enableCors({
+    origin: ['http://localhost:8080', 'http://89.116.228.185', 'https://89.116.228.185', 'https://sagenicolas.fr', 'http://89.116.228.185/commerceclient/'],
+})
+app.use(json({ limit: '50mb' }));
+app.use(urlencoded({ extended: true, limit: '50mb' }));
+const config = new DocumentBuilder()
+    .setTitle('Ecommerce example')
+    .setDescription('The ecommerce API control')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+await app.listen(3000);
 }
 bootstrap();
