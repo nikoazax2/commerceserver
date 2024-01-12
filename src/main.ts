3caabf79d39fd41d3c473a031d17ee776397c155
nikoazax2 <nicolasage38@orange.fr>
2024-01-12 17:03:12 +0100

    add

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,12 @@ async function bootstrap() {
     let ssl = {
         httpsOptions: {
             key: fs.readFileSync('./secrets/privkey.pem'),
-            cert: fs.readFileSync('./secrets/fullchain.pem'),
+            cert: fs.readFileSync('./secrets/fullchain.pem')
         }
     }
     const app = await NestFactory.create(AppModule, ssl)
     app.enableCors({
-        origin: ['http://localhost:8080', 'http://localhost:8080/', 'https://localhost:8080/', 'http://89.116.228.185', 'https://89.116.228.185', 'https://sagenicolas.fr', 'http://sagenicolas.fr', 'https://sagenicolas.fr/commerceclient/', 'http://89.116.228.185/commerceclient/'],
+        origin: ['http://localhost:8080', 'http://localhost:3000/categorie', 'http://localhost:8080/', 'https://localhost:8080/', 'http://89.116.228.185', 'https://89.116.228.185', 'https://sagenicolas.fr', 'http://sagenicolas.fr', 'https://sagenicolas.fr/commerceclient/', 'http://89.116.228.185/commerceclient/'],
     })
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ extended: true, limit: '50mb' }));
