c20cc1e7254a82a7c66106872c7a1aa70c5d0721
nikoazax2 <nicolasage38@orange.fr>
2024-01-23 20:59:21 +0100

    add

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ extended: true, limit: '50mb' })); 
-    app.enableCors();
+ //   app.enableCors();
 
     const config = new DocumentBuilder()
         .setTitle('Ecommerce example')
